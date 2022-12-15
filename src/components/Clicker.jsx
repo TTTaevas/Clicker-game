19c0f4abdf1e00ac1db20e7ee63f24d85cb5dfa5
Rémi Lagreze <remilagr02@gmail.com>
2022-12-15 16:04:14 +0100

    Counter working, just need to multiply the price of the clicker

--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-
+import increment from "../../utils/utils";
 export default function Clicker({ setScore, score }) {
   const [clicker, setClicker] = useState(0);
+
   const handleBuyClicker = () => {
     if (score >= 10) {
       setScore(score - 10);
       setClicker(clicker + 1);
+      setInterval(() => setScore((oldScore) => oldScore + 1), 1000);
     }
   };
   return (

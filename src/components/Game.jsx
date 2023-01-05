ca63417bc4242c3a50232e75a6e44b237809bbb0
Rémi Lagreze <remilagr02@gmail.com>
2023-01-05 10:06:49 +0100

    Added zones counter, added functional bosses, but no timer and no circles to click.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "../style/score.css";
 import "../style/progressbar.css";
 import blob from "../../assets/blob.png";
@@ -7,7 +7,7 @@ import Experiencebar from "./Experiencebar";
 import Zones from "./Zones";
 
 export default function Game(props) {
-  let [power, setPower] = useState(1);
+  let [power, setPower] = useState(10000);
   let [score, setScore] = useState(0);
   let [maxLife, setMaxLife] = useState(10);
   let [life, setLife] = useState(maxLife);
@@ -46,7 +46,7 @@ export default function Game(props) {
       <progress max={maxLife} value={life} className="healthbar" />
       <p>{life} HP</p>
       <Clicker score={score} setLife={setLife} setScore={setScore} />
-      <p className="score">{Math.round(score * 10) / 10}</p>
+      <p className="score">{Math.round(score)}</p>
     </div>
   );
 }
--- src/components/Zones.jsx
+++ src/components/Zones.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "../style/zones.css";
 
 export default function Zones({
   score,
@@ -14,22 +15,34 @@ export default function Zones({
   const [maxMonsterCount, setMaxMonsterCount] = useState(10);
   const spawnMonster = () => {
     if (monsterCount === maxMonsterCount - 1) {
-      setMaxLife(maxLife = Math.round(maxLife * 1.2));
-      setMonsterCount(0);
-      setMonsterZone(Math.round(monsterZone * 11.2) / 10);
+      if (monsterZone % 9 === 0) {
+        setMaxLife((maxLife = Math.round(maxLife * 60)));
+        setMonsterCount(0);
+        setMaxMonsterCount(1);
+        setMonsterZone(monsterZone + 1);
+      } else {
+        if (monsterZone % 10 === 0) {
+          setMaxLife((maxLife = Math.round(maxLife / 30)));
+        }
+        setMaxLife((maxLife = Math.round(maxLife * 1.8)));
+        setMonsterCount(0);
+        setMonsterZone(monsterZone + 1);
+        setMaxMonsterCount(10);
+      }
     }
     setLife((life = maxLife));
-  }
+  };
   useEffect(() => {
     if (life <= 0) {
-      setScore(score + monsterZone);
+      setScore(score + maxLife / 6);
       setMonsterCount(monsterCount + 1);
       spawnMonster();
     }
   });
   return (
-    <div>
-      {monsterCount}/{maxMonsterCount}
+    <div className="monsterCount">
+      {monsterCount}/{maxMonsterCount} <br />
+      You are in zone {monsterZone}
     </div>
   );
 }

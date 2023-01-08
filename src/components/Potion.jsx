2cd511b1a550d1ca7ce212868f6d2273f87a6c12
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-01-08 01:53:49 +0100

    Add countdown to potion

--- src/components/Potion.jsx
+++ src/components/Potion.jsx
@@ -7,20 +7,39 @@ export default function Potion({
   setExperience,
 }) {
   const [price, setPrice] = useState(100000);
+  let [length, setLength] = useState(75);
 
   const buyPotion = () => {
-    setPotion(true);
-    setTimeout(() => {
-      setPotion(false);
-    }, 15 * 60 * 1000);
+    if (!potion) {
+      let countdown = setInterval(() => setLength((length) => length - 1), 1000)
+      setPotion(true);
+      setTimeout(() => {
+        setPotion(false);
+        clearInterval(countdown);
+        setLength(length); // Because it's in timeout, length is the original value
+      }, length * 1000);
+    }
   };
 
   return (
     <>
-      {potion && <p>You currently are under effect of a potion!</p>}
+      {potion && <p>The potion's effects will dissipate in {getLengthInWrittenForm(length)}</p>}
       <button type="button" onClick={() => buyPotion()}>
-        Buy potion : {Math.round(price)} points
+        Buy potion: {Math.round(price)} points
       </button>
     </>
   );
+}
+
+function getLengthInWrittenForm(length) {
+  let minutes = 0;
+  let seconds = 0;
+  for (let i = length; i >= 60; i -= 60) {
+    minutes++;
+    length -= 60
+  };
+  for (let i = 0; i < length; i++) {
+    seconds++;
+  };
+  return `${minutes}:${String(seconds).padStart(2, "0")}`;
 }

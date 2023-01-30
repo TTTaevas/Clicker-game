b545ad7e941ca5115e78f7bd123ee4461e8c9f22
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-01-30 12:53:11 +0100

    Fix enchantment alerts

--- src/components/Enchants.jsx
+++ src/components/Enchants.jsx
@@ -1,10 +1,48 @@
 import "../style/shop.css";
 
 export default function Enchants({
+  score,
+  setScore,
+  swords,
+  setSwords,
+  makeSwordDealDamage,
+  intervalId,
+  clearInterval,
   enchantsPrice,
-  handleBuyEnchant,
-  swords
+  setEnchantsPrice
 }) {
+  const handleBuyEnchant = (selectedSword) => {
+    if (score >= Math.round(enchantsPrice)) {
+      setScore(score - Math.round(enchantsPrice));
+      setEnchantsPrice(enchantsPrice * 1.2);
+      const updatedSwords = swords.map((s) => {
+        if (s.id === parseInt(selectedSword)) {
+          s.enchant = Math.floor(Math.random() * 1000);
+          if (s.enchant === 727) {
+            alert("Legendary Enchant: WYSI! Damage of this sword +727%");
+            if (s.equipped) {
+              clearInterval(intervalId);
+              makeSwordDealDamage({ ...s, damage: Math.round(s.damage * 8.27) });
+            }
+            return { ...s, damage: Math.round(s.damage * 1.1) };
+          } else if (s.enchant > 500) {
+            alert("Common enchant: Damage of this sword +10%!");
+            if (s.equipped) {
+              clearInterval(intervalId);
+              makeSwordDealDamage({ ...s, damage: Math.round(s.damage * 1.1) });
+            }
+            return { ...s, damage: Math.round(s.damage * 1.1) };
+          } else if (s.enchant <= 500) {
+            alert("Common enchant: Price of this sword -50%!");
+            return { ...s, price: Math.round(s.price * 0.5) };
+          }
+        }
+        return s;
+      });
+      setSwords(updatedSwords);
+    }
+  };
+
   const swordsToEnchant = swords.filter((s) => s.bought && s.enchant < 1)
   return (
     <div className="enchantsTab">

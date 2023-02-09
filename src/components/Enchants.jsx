623f696cc3620a5f83ba0dcbd669f88e804bad8b
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-02-09 03:33:52 +0100

    Introduce two new sword enchantments

--- src/components/Enchants.jsx
+++ src/components/Enchants.jsx
@@ -26,16 +26,22 @@ export default function Enchants({
               makeSwordDealDamage({ ...s, damage: s.damage * 8.27 });
             }
             return { ...s, damage: s.damage * 8.27 };
-          } else if (s.enchant > 500) {
+          } else if (s.enchant <= 300) {
             alert("Common enchant: Damage of this sword +10%!");
             if (s.equipped) {
               clearInterval(intervalId);
               makeSwordDealDamage({ ...s, damage: s.damage * 1.1 });
             }
             return { ...s, damage: s.damage * 1.1 };
-          } else if (s.enchant <= 500) {
+          } else if (s.enchant <= 600) {
             alert("Common enchant: Price of this sword -50%!");
             return { ...s, price: Math.round(s.price * 0.5) };
+          } else if (s.enchant <= 900) {
+            alert("Common enchant: Higher level cap for this sword!");
+            return { ...s, levelCapMultiplier: Math.round(s.levelCapMultiplier * 1.3) };
+          } else if (s.enchant <= 1000) {
+            alert("Rare enchant: This sword generates gold while equipped!!");
+            return { ...s, goldChance: Math.round(s.goldChance += 1) };
           }
         }
         return s;

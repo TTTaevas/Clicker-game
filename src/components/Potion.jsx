bae80d1c0cf4f3d239693f112574bb40414bba8f
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-02-09 19:49:27 +0100

    Better shop display

--- src/components/Potion.jsx
+++ src/components/Potion.jsx
@@ -36,15 +36,18 @@ export default function Potion({
   return (
     <>
       <button className={potionStyle} type="button" onClick={() => buyPotion(1)}>
-        Buy XP potion: {displayNumber(price)} gold{" "}
+        Buy XP potion:<br />
+        {displayNumber(price)} gold{" "}
           <img className="coinIcon" src={coinIcon} />
       </button>
       <button className={potionStyle} type="button" onClick={() => buyPotion(2)}>
-        Buy gold potion: {displayNumber(price)} gold{" "}
+        Buy gold potion:<br />
+        {displayNumber(price)} gold{" "}
           <img className="coinIcon" src={coinIcon} />
       </button>
       <button className={potionStyle} type="button" onClick={() => buyPotion(3)}>
-        Buy strength potion: {displayNumber(price)} gold{" "}
+        Buy strength potion:<br />
+        {displayNumber(price)} gold{" "}
           <img className="coinIcon" src={coinIcon} />
       </button>
     </>

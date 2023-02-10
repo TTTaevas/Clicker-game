bbe120f263d2eb95f70e56bf3421d04429af8471
RemiL-Nel <113448454+RemiL-Nel@users.noreply.github.com>
2023-02-10 09:57:36 +0100

    Revert "Save the player's progress!"

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import "../style/game.css";
-import "../style/experience.css";
 import "../style/progressbar.css";
 import blob from "../../assets/blob.png";
 import hurtblob from "../../assets/hurtBlob.png";
@@ -13,6 +12,7 @@ import hurtskeleton from "../../assets/hurtSkeleton.png";
 import deadskeleton from "../../assets/hurtSkeleton.png";
 import target from "../../assets/target.png";
 import Shop from "./Shop";
+import Experiencebar from "./Experiencebar";
 import Zones from "./Zones";
 import Debug from "./Debug";
 
@@ -27,17 +27,15 @@ export default function Game() {
   const [currentMobClass, setCurrentMobClass] = useState("blob");
   const [blobState, setBlobState] = useState(0);
   const [nextZoneText, setNextZoneText] = useState(false);
-  const [maxMonsterCount, setMaxMonsterCount] = useState(Number(localStorage.getItem("maxMonsterCount")) || 5);
-  let [level, setLevel] = useState(Number(localStorage.getItem("level")) || 1);
-
+  const [maxMonsterCount, setMaxMonsterCount] = useState(5);
+  let [level, setLevel] = useState(1);
   let [cps, setCps] = useState(0);
-  let [power, setPower] = useState(Number(localStorage.getItem("power")) || 1);
-  let [score, setScore] = useState(Number(localStorage.getItem("score")) || 0);
-  let [maxLife, setMaxLife] = useState(Number(localStorage.getItem("maxLife")) || 10);
-  let [life, setLife] = useState(Number(localStorage.getItem("life")) || maxLife);
-  let [experience, setExperience] = useState(Number(localStorage.getItem("experience")) || 0);
-  let [maxExperience, setMaxExperience] = useState(Number(localStorage.getItem("maxExperience")) || 100);
-  let [monsterZone, setMonsterZone] = useState(Number(localStorage.getItem("monsterZone")) || 1);
+  let [power, setPower] = useState(1);
+  let [score, setScore] = useState(0);
+  let [maxLife, setMaxLife] = useState(10);
+  let [life, setLife] = useState(maxLife);
+  let [experience, setExperience] = useState(0);
+  let [monsterZone, setMonsterZone] = useState(1);
   const [imagePosition, setImagePosition] = useState({ x: 100, y: 100 });
   const [containerDimensions, setContainerDimensions] = useState({
     width: 0,
@@ -51,23 +49,7 @@ export default function Game() {
       height: container.offsetHeight,
     });
   }, []);
-  useEffect(() => {localStorage.setItem("maxMonsterCount", maxMonsterCount)}, [maxMonsterCount])
-  useEffect(() => {localStorage.setItem("level", level)}, [level])
-  useEffect(() => {localStorage.setItem("power", power)}, [power])
-  useEffect(() => {localStorage.setItem("score", score)}, [score])
-  useEffect(() => {localStorage.setItem("maxLife", maxLife)}, [maxLife])
-  useEffect(() => {localStorage.setItem("life", life)}, [life])
-  useEffect(() => {localStorage.setItem("monsterZone", monsterZone)}, [monsterZone])
-  useEffect(() => {
-    if (experience >= maxExperience) {
-      setExperience(0);
-      setMaxExperience(Math.round(maxExperience * 1.4));
-      setLevel(level + 1);
-      setPower(power + 1);
-    }
-    localStorage.setItem("experience", experience)
-  }, [experience])
-  useEffect(() => {localStorage.setItem("maxExperience", maxExperience)}, [maxExperience])
+
   const displayNumber = (num) => {
     let str = String(Math.round(num));
     if (str.length < 4) {
@@ -213,25 +195,6 @@ export default function Game() {
             setLevel={setLevel}
           />
           <div className="clickzone" ref={containerRef}>
-          <button
-            type="button"
-            onClick={() => attackMonster()}
-            className="blob"
-          >
-            <img
-              src={blob}
-              alt="monster"
-              className="blob"
-              style={{ display: blobClicked ? "none" : "inline" }}
-            />
-            <img
-              src={hurtBlob}
-              alt="monster"
-              className="blob"
-              style={{ display: blobClicked ? "inline" : "none" }}
-            />
-          </button>
-          {monsterZone % 10 === 0 && (
             <button
               type="button"
               onClick={() => attackMonster()}
